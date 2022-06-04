dd41751f5280580d4c3fbc6bac85673ecd6a420e
printf-joaova <82693724+printf-joaova@users.noreply.github.com>
2022-06-04 17:55:17 -0300

    Update anima.js

--- anima.js
+++ anima.js
@@ -1,19 +1,3 @@
-let cardTransitionTime = 500;
-
-let $card = $('.js-card')
-let switching = false
-
-$('.card').click(flipCard)
-
-function flipCard () {
-   if (switching) {
-      return false
-   }
-   switching = true
-   
-   $card.toggleClass('is-switched')
-   window.setTimeout(function () {
-      $card.children().children().toggleClass('is-active')
-      switching = false
-   }, cardTransitionTime / 2)
-}
+$(".card").click(function() {
+  $(this).closest(".card").toggleClass("flipped");
+});
